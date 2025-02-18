d2887353f76ae2f3ddf2faf94c9f6534a11b6e2c
jsantiag <jocelyn.santiag@gmail.com>
2025-02-18 17:42:02 -0500

    what is this

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Provider } from 'react-redux';
 // import { ThemeContext, getTheme } from 'react-native-material-ui';
 // import { PersistGate } from 'redux-persist/integration/react';
-
+//adding silly little comments
 import { store } from './src/store';
 // import { uiTheme } from './src/styles/theme';
 import AppNavigator from './src/navigator';
+//more silly little comments for testing
 
 export default class App extends React.Component {
   render() {
